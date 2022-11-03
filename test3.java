adea558d3c3a2c70d5458f120dd068c5118e47b7
Gomathy19 <93815964+Gomathy19@users.noreply.github.com>
2022-11-03 18:23:39 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -4,10 +4,9 @@ public class test3 {
 	public static void main(String[] args) {
 		
 		String s = new String ("Hello Gomathy");
-		String s1 = new String ("GitHUB")
 		
 		System.out.println(s);
-		System.out.println(s1);
+		
 		
 
 	}
