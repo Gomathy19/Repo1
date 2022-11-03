b373eec3e6c281fc895c3ba796b1bb55bf6fdfe9
Gomathy@19 <gomathygoms19@gmail.com>
2022-11-03 18:12:42 +0530

    New changes made

--- test3.java
+++ test3.java
@@ -4,8 +4,10 @@ public class test3 {
 	public static void main(String[] args) {
 		
 		String s = new String ("Hello Gomathy");
+		String s1 = new String ("GitHUB")
 		
 		System.out.println(s);
+		System.out.println(s1);
 		
 
 	}
