80f29a47ac481f90ef557ec5edb7e948c02c89da
Gomathy19 <93815964+Gomathy19@users.noreply.github.com>
2022-11-03 18:00:49 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -3,7 +3,7 @@ public class test3 {
 
 	public static void main(String[] args) {
 		
-		String s = new String ("Hello Java");
+		String s = new String ("Hello Gomathy");
 		
 		System.out.println(s);
 		
