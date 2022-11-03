2b23d7c6f4384139de103578fc12bca92f89c16c
Gomathy@19 <gomathygoms19@gmail.com>
2022-11-03 18:44:52 +0530

    Updated s1 value again in local

--- test3.java
+++ test3.java
@@ -4,7 +4,7 @@ public class test3 {
 	public static void main(String[] args) {
 		
 		String s = new String ("Hello Gomathy");
-		String s1 = new String ("Welcome to TutorialsPoint");
+		String s1 = new String ("Welcome to Java");
 		System.out.println(s);
 		System.out.println(s1);
 		
